b3b89be2181eb1ac3c6d3ea45200be06ed4cabd9
karinyasuda <karin.03k35@gmail.com>
2015-10-28 22:43:55 +0900

    white

--- hanguk/QuizViewController.swift
+++ hanguk/QuizViewController.swift
@@ -15,19 +15,13 @@ class QuizViewController: UIViewController {
     var quizArray  = [AnyObject]()
     
     //出題数
-    var questionNumber:Int = 1
+    var questionNumber:Int = 10
     
     //現在の問題数
     var sum:Int = 0
     
     //正解数
     var correctAnswer:Int = 0
-//    var falseAnswer:Int = 0
-    
-    
-    //乱数
-//    var random:Int = 0
-    
     
     //クイズの問題を表示するtextview
     @IBOutlet var questionTextView:UITextView!
@@ -44,10 +38,16 @@ class QuizViewController: UIViewController {
         var qArray = [AnyObject]()
         
         //クイズの問題　30問分の型の用意
-        qArray.append(["6人前","육인분","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
+        qArray.append(["6人前","육인분","여섯인분","일곱인분","유인",1])
+        qArray.append(["8枚","팔츤","여덟장","팔층","여덟면",2])
+        qArray.append(["2個","이것","이개","두개","둘캐",3])
+        qArray.append(["9台","","","","",4])
+        qArray.append(["5階","","","","",3])
+        
+        qArray.append(["10月27日","","","","",])
+        qArray.append(["4泊","","","","",])
+        qArray.append(["봄이(　　　)꽃이 핍니다　春が来れば花が咲きます。","오기 전에","오다","오면","오로",3])
+        qArray.append(["(　　　)오십시요","오른쯕으로","압으면","앞어","앞으로",4])
         qArray.append(["","","","","",])
         
         qArray.append(["","","","","",])
@@ -74,14 +74,8 @@ class QuizViewController: UIViewController {
         qArray.append(["","","","","",])
         qArray.append(["","","","","",])
         
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
         
-        
-        
+        //問題をシャッフルしてquizarrayにいれる！格納する！！
         while (qArray.count > 0){
             let index = Int(arc4random_uniform(UInt32(qArray.count)))
             quizArray.append(qArray[index])
@@ -104,6 +98,8 @@ class QuizViewController: UIViewController {
 
         }
     @IBAction func choiceAnswer(sender: UIButton) {
+        sum++
+        
         if quizArray[0][4] as! Int == sender.tag {
             //正解数を増やす
             correctAnswer++
