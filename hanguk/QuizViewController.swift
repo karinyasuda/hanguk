8928ef72bf9b10999f41bc5efd8937183e356b33
karinyasuda <karin.03k35@gmail.com>
2015-10-28 14:50:57 +0900

    韓国語表示された！

--- hanguk/QuizViewController.swift
+++ hanguk/QuizViewController.swift
@@ -11,15 +11,28 @@
 import UIKit
 
 class QuizViewController: UIViewController {
-    
+    //クイズの問題を入れる配列
     var quizArray  = [AnyObject]()
     
-    var correctAnswer:Int = 0
-    var falseAnswer:Int = 0
+    //出題数
+    var questionNumber:Int = 1
     
+    //現在の問題数
+    var sum:Int = 0
+    
+    //正解数
+    var correctAnswer:Int = 0
+//    var falseAnswer:Int = 0
+    
+    
+    //乱数
+//    var random:Int = 0
+    
+    
+    //クイズの問題を表示するtextview
     @IBOutlet var questionTextView:UITextView!
     
-    
+//    @IBOutlet var choiceButtons: Array<UIButton>!
     @IBOutlet var Button1:UIButton!
     @IBOutlet var Button2:UIButton!
     @IBOutlet var Button3:UIButton!
@@ -31,13 +44,7 @@ class QuizViewController: UIViewController {
         var qArray = [AnyObject]()
         
         //クイズの問題　30問分の型の用意
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        qArray.append(["","","","","",])
-        
-        qArray.append(["","","","","",])
+        qArray.append(["6人前","육인분","","","",])
         qArray.append(["","","","","",])
         qArray.append(["","","","","",])
         qArray.append(["","","","","",])
@@ -66,7 +73,13 @@ class QuizViewController: UIViewController {
         qArray.append(["","","","","",])
         qArray.append(["","","","","",])
         qArray.append(["","","","","",])
-        //
+        
+        qArray.append(["","","","","",])
+        qArray.append(["","","","","",])
+        qArray.append(["","","","","",])
+        qArray.append(["","","","","",])
+        qArray.append(["","","","","",])
+        
         
         
         while (qArray.count > 0){
@@ -79,6 +92,7 @@ class QuizViewController: UIViewController {
     }
     
         func choiceQuiz() {
+            print(quizArray.count)
             questionTextView.text = quizArray[0][0] as! String
             //選択肢のボタン
             
@@ -89,39 +103,37 @@ class QuizViewController: UIViewController {
             Button3.setTitle( quizArray [0][3] as? String, forState: .Normal)
 
         }
-        
-    @IBAction func choiceAnswer(sender: UIButton){
-        if quizArray[0][4] as! Int == sender.tag{
-        
-        correctAnswer++
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer++
         }
         
         quizArray.removeAtIndex(0)
-        if quizArray.count == 0{
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
             performSegueToResult()
         } else {
             choiceQuiz()
         }
-        
-        }
-    func performSegueToResult(){
-        
-        
-        
     }
-        
     
+    func performSegueToResult() {
+        performSegueWithIdentifier("toResultView", sender: nil)
+    }
     
-        
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destinationViewController as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
     
-    
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-    
-    }
-
+        // Dispose of any resources that can be recreated.
 }
 
 
+}
---
+++ hanguk/ResultViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ResultViewController.swift
+//  hanguk
+//
+//  Created by Karin on 2015/10/28.
+//  Copyright © 2015年 Karin. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var correctAnswer:Int = 0
+    @IBOutlet var resultlabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        
+        resultlabel.text = "\(correctAnswer)"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
